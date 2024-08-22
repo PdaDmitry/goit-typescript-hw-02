c45aa78abfc27e03ac01935342ffa70639a7e1dc
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2024-08-22 17:09:49 +0300

    Update Loader.tsx

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -5,20 +5,17 @@ import css from './Loader.module.css';
 export default function Loader() {
   return (
     <div className={css.loaderCont}>
-      {/* <RotatingLines */}
+      <RotatingLines
         visible={true}
-        height={20}
-        width={20}
-        color="grey"
+        // height={20}
+        width="20"
+        // color="grey"
         strokeWidth="5"
         animationDuration="0.75"
         ariaLabel="rotating-lines-loading"
-        {/* wrapperStyle={{}} */}
-        wrapperClass=""
-      {/* /> */}
+        // wrapperClass=""
+      />
       <p className={css.loaderText}> Loading photos, please wait...</p>
     </div>
   );
-};
-
-// export default Loader;
+}
