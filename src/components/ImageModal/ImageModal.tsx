6d8fa47d1574ef2f05f44c1fc63685deb00ba58d
PdaDmitry <pda.gazcomp@gmail.com>
2024-08-22 12:26:05 +0300

    Update Loader.tsx and api.ts

--- src/components/ImageModal/ImageModal.tsx
+++ src/components/ImageModal/ImageModal.tsx
@@ -4,25 +4,21 @@ import { Image } from '../ImageGallery/ImageGallery.types';
 
 
 interface ImageModalProps {
-  item: Image;
-  alt_description: string;
+  item: Image|null;
   isOpen: boolean;
   onRequestClose: () => void;
   style: object;
 }
 
 const ImageModal: FC<ImageModalProps> = ({
-  item: {
-    urls: { regular },
-  },
-  alt_description,
+  item,
   isOpen,
   onRequestClose,
   style,
 }) => {
   return (
     <Modal isOpen={isOpen} onRequestClose={onRequestClose} style={style}>
-      <img src={regular} alt={alt_description} />
+      <img src={item?.urls.regular} alt={item?.alt_description||'Image'} />
     </Modal>
   );
 };
--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,24 +1,24 @@
 import { RotatingLines } from 'react-loader-spinner';
 import css from './Loader.module.css';
-import React from 'react';
+// import React from 'react';
 
-const Loader: React.FC = () => {
+export default function Loader() {
   return (
     <div className={css.loaderCont}>
-      <RotatingLines
+      {/* <RotatingLines */}
         visible={true}
-        height="20"
-        width="20"
+        height={20}
+        width={20}
         color="grey"
         strokeWidth="5"
         animationDuration="0.75"
         ariaLabel="rotating-lines-loading"
-        wrapperStyle={{}}
+        {/* wrapperStyle={{}} */}
         wrapperClass=""
-      />
+      {/* /> */}
       <p className={css.loaderText}> Loading photos, please wait...</p>
     </div>
   );
 };
 
-export default Loader;
+// export default Loader;
