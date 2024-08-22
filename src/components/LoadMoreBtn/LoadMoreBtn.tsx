5c538cd83d4a82dc297ab7d46fc0d5580b87af25
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2024-08-22 18:45:44 +0300

    LoadMoreBtn.tsx

---
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+import css from './LoadMoreBtn.module.css';
+
+interface LoadMoreBtnProps {
+  onClick: () => void;
+}
+
+const LoadMoreBtn: FC<LoadMoreBtnProps> = ({ onClick }) => {
+  return (
+    <div>
+      <button className={css.btnLoad} onClick={onClick}>
+        Load more
+      </button>
+    </div>
+  );
+};
+
+export default LoadMoreBtn;
